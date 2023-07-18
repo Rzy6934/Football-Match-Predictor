3961ac6017a7765d14dc7f4845b2f4f0ce720f85
Rzy6934 <ramzy-amirat@hotmail.com>
2023-07-18 23:44:04 +0200

    Add of January, February and March data

--- scrapper_3.py
+++ scrapper_3.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
         selectable_months = get_selectable_months(driver)
         print(selectable_months)
         time.sleep(1)
-        for month in selectable_months[0:1]:
+        for month in selectable_months[3:4]:
             select_month(driver, month)
             time.sleep(1)
             month_games = get_month_games(driver)
@@ -231,11 +231,11 @@ if __name__ == "__main__":
             month_games = []
                 
     add_full_time_results(all_games_data)
-    add_matchday(all_games_data, 183)
+    add_matchday(all_games_data, 288)
 
     json_data = json.dumps(all_games_data)
 
-    with open("Jan_2022_data.json", "w") as json_file:
+    with open("Apr_2022_data.json", "w") as json_file:
         json_file.write(json_data)
 
     # print(all_games_data)
