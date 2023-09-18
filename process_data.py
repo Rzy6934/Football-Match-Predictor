7c64adfe00ad9c6d58f122a4c8571a16cab990d7
Rzy6934 <ramzy-amirat@hotmail.com>
2023-09-18 06:44:25 +0200

    Add function that get 5 and 3 wins and losses streaks stats

--- process_data.py
+++ process_data.py
@@ -16,7 +16,7 @@ def get_teams(all_games_data, nb_week_games):
         teams.append(game_data[2])
 
 def add_default_stats(all_games_data):
-    zeros_list = [0, 0, 0, 0, 0, 0, 0, 0]
+    zeros_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for game_data in all_games_data:
         game_data += zeros_list
 
@@ -121,9 +121,10 @@ def get_win_losses(all_games_data):
 
     return all_teams_wins_losses
 
-def get_5g_streak(all_teams_wins_losses):
+def get_5g_streaks(all_teams_wins_losses):
     all_teams_5g_streaks = []
     team_5g_streaks = []
+    
     for team_wins_losses in all_teams_wins_losses:
         for i in range(len(team_wins_losses)):
             if i < 5:
@@ -140,21 +141,83 @@ def get_5g_streak(all_teams_wins_losses):
 
     return all_teams_5g_streaks
 
-def add_agg_stats(all_games_data, all_teams_agg_goals_scored, all_teams_agg_goals_conceded, all_teams_agg_points, teams):
+def get_5_wins_losses_streaks(all_teams_5g_streaks):
+    all_teams_5_wins_streaks = []
+    all_teams_5_losses_streaks = []
+    team_5_wins_streaks = []
+    team_5_losses_streaks = []
+    five_wins_streaks = 0
+    five_losses_streaks = 0
+    
+    for team_5g_streaks in all_teams_5g_streaks:
+        for streak in team_5g_streaks:
+            if streak == "WWWWW":
+                five_wins_streaks += 1
+            elif streak == "LLLLL":
+                five_losses_streaks += 1
+            team_5_wins_streaks.append(five_wins_streaks)
+            team_5_losses_streaks.append(five_losses_streaks)
+        
+        all_teams_5_wins_streaks.append(team_5_wins_streaks)
+        all_teams_5_losses_streaks.append(team_5_losses_streaks)
+        team_5_wins_streaks = []
+        team_5_losses_streaks = []
+        five_wins_streaks = 0
+        five_losses_streaks = 0
+        
+    return all_teams_5_wins_streaks, all_teams_5_losses_streaks
+
+def get_3_wins_losses_streaks(all_teams_5g_streaks):
+    all_teams_3_wins_streaks = []
+    all_teams_3_losses_streaks = []
+    team_3_wins_streaks = []
+    team_3_losses_streaks = []
+    three_wins_streaks = 0
+    three_losses_streaks = 0
+    
+    for team_5g_streaks in all_teams_5g_streaks:
+        for streak in team_5g_streaks:
+            if streak[-3:] == "WWW":
+                three_wins_streaks += 1
+            elif streak[-3:] == "LLL":
+                three_losses_streaks += 1
+            team_3_wins_streaks.append(three_wins_streaks)
+            team_3_losses_streaks.append(three_losses_streaks)
+        
+        all_teams_3_wins_streaks.append(team_3_wins_streaks)
+        all_teams_3_losses_streaks.append(team_3_losses_streaks)
+        team_3_wins_streaks = []
+        team_3_losses_streaks = []
+        three_wins_streaks = 0
+        three_losses_streaks = 0
+        
+    return all_teams_3_wins_streaks, all_teams_3_losses_streaks
+            
+def add_agg_stats(all_games_data, all_teams_agg_goals_scored, all_teams_agg_goals_conceded, all_teams_agg_points, all_teams_5g_streaks, all_teams_5_wins_streaks, all_teams_5_losses_streaks, all_teams_3_wins_streaks, all_teams_3_losses_streaks, teams):
     counter = 0
-    for team, agg_goals_scored, agg_goals_conceded, agg_points in zip(teams, all_teams_agg_goals_scored, all_teams_agg_goals_conceded, all_teams_agg_points):
+    for team, agg_goals_scored, agg_goals_conceded, agg_points, team_5g_streaks, team_5_wins_streaks, team_5_losses_streaks, team_3_wins_streaks, team_3_losses_streaks in zip(teams, all_teams_agg_goals_scored, all_teams_agg_goals_conceded, all_teams_agg_points, all_teams_5g_streaks, all_teams_5_wins_streaks, all_teams_5_losses_streaks, all_teams_3_wins_streaks, all_teams_3_losses_streaks):
         for game_data in all_games_data:
             if team in game_data[1]:
                 game_data[51] = agg_goals_scored[counter]
                 game_data[53] = agg_goals_conceded[counter]
                 game_data[55] = agg_points[counter]
                 game_data[57] = agg_goals_scored[counter] - agg_goals_conceded[counter]
+                game_data[59] = team_5g_streaks[counter]
+                game_data[61] = team_5_wins_streaks[counter]
+                game_data[63] = team_5_losses_streaks[counter]
+                game_data[65] = team_3_wins_streaks[counter]
+                game_data[67] = team_3_losses_streaks[counter]
                 counter += 1
             elif team in game_data[2]:
                 game_data[52] = agg_goals_scored[counter]
                 game_data[54] = agg_goals_conceded[counter]
                 game_data[56] = agg_points[counter]
                 game_data[58] = agg_goals_scored[counter] - agg_goals_conceded[counter]
+                game_data[60] = team_5g_streaks[counter]
+                game_data[62] = team_5_wins_streaks[counter]
+                game_data[64] = team_5_losses_streaks[counter]
+                game_data[66] = team_3_wins_streaks[counter]
+                game_data[68] = team_3_losses_streaks[counter]
                 counter += 1
         counter = 0
 
@@ -175,20 +238,24 @@ def add_agg_stats(all_games_data, all_teams_agg_goals_scored, all_teams_agg_goal
 
 
 if __name__ == "__main__":
-    while True:
-        championship_input = input("Championship : ")
-        if championship_input in championships:
-            break
-        else:
-            print("Invalid championship. Please enter a valid championship.")
+    # while True:
+    #     championship_input = input("Championship : ")
+    #     if championship_input in championships:
+    #         break
+    #     else:
+    #         print("Invalid championship. Please enter a valid championship.")
 
-    while True:
-        season_input = input("Season : ")
-        if season_input in seasons:
-            break
-        else:
-            print("Invalid season. Please enter a valid season.")
+    # while True:
+    #     season_input = input("Season : ")
+    #     if season_input in seasons:
+    #         break
+    #     else:
+    #         print("Invalid season. Please enter a valid season.")
 
+    # season_formatted = transform_season_format(season_input)
+    
+    championship_input = "LaLiga"
+    season_input = "2022-2023"
     season_formatted = transform_season_format(season_input)
 
     json_data_path = f"data/{championship_input}/{season_input}/{season_formatted}_data.json"
@@ -208,10 +275,16 @@ if __name__ == "__main__":
     # add_matchday(all_games_data, nb_week_games)
 
     get_teams(all_games_data, nb_week_games)
+    
+    print(teams)
 
     all_teams_wins_losses = get_win_losses(all_games_data)
 
-    all_teams_5g_streak = get_5g_streak(all_teams_wins_losses)
+    all_teams_5g_streaks = get_5g_streaks(all_teams_wins_losses)
+    
+    all_teams_5_wins_streaks, all_teams_5_losses_streaks = get_5_wins_losses_streaks(all_teams_5g_streaks)
+    
+    all_teams_3_wins_streaks, all_teams_3_losses_streaks = get_3_wins_losses_streaks(all_teams_5g_streaks)
 
     all_teams_agg_goals_scored, all_teams_agg_goals_conceded = get_agg_goals(all_games_data)
     
@@ -219,10 +292,8 @@ if __name__ == "__main__":
 
     all_games_data_updated = add_default_stats(all_games_data)
 
-    all_games_data_updated_2 = add_agg_stats(all_games_data_updated, all_teams_agg_goals_scored, all_teams_agg_goals_conceded, all_teams_agg_points, teams)
+    all_games_data_updated_2 = add_agg_stats(all_games_data_updated, all_teams_agg_goals_scored, all_teams_agg_goals_conceded, all_teams_agg_points, all_teams_5g_streaks, all_teams_5_wins_streaks, all_teams_5_losses_streaks, all_teams_3_wins_streaks, all_teams_3_losses_streaks, teams)
 
-    # print(all_games_data_updated_2[304])
-    print(all_teams_wins_losses[0])
-    print(all_teams_5g_streak[0])
-
-    
+    for data in all_games_data_updated_2[296:]:
+        print("--------------------------------")
+        print(data)
